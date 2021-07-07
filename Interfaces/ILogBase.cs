41e9989199f0c726e417debc8df57b7a3bc798fd
Gabriel de Paula <gabrieldepaula007@gmail.com>
2021-07-07 11:59:24 -0300

    Add: Interface ILogBase

---
+++ Interfaces/ILogBase.cs
@@ -0,0 +1,14 @@
+﻿using AppLogger.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLogger.Interfaces
+{
+    public interface ILogBase
+    {
+        T CreateEntity<T>(Enums.EntityType entityType) where T : new();
+    }
+}
